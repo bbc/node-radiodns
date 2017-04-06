330acc5826c7f0455264517bcdeb5fceb37e65d3
Nicholas Humfrey <njh@aelius.com>
2017-04-06 17:55:26 +0100

    Implemented radiodns.constructFQDN(params)

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# node-radiodns
-Perform RadioDNS resolutions and service lookups in node.js
+[![Build Status](https://travis-ci.org/bbc/node-radiodns.svg?branch=master)](https://travis-ci.org/bbc/node-radiodns)
+
+
+RadioDNS node.js Library
+========================
+
+## Summary
+
+Perform [RadioDNS] resolutions and service lookups in [node.js].
+
+RadioVis was standardised in [ETSI TS 103 270], _RadioDNS Hybrid Radio; Hybrid lookup for radio services_.
+
+
+## Installation
+
+    npm install --save radiodns
+
+
+
+
+[node.js]:         https://nodejs.org/
+[RadioDNS]:        https://en.wikipedia.org/wiki/RadioDNS
+[ETSI TS 103 270]: http://www.etsi.org/deliver/etsi_ts/103200_103299/103270/01.01.01_60/ts_103270v010101p.pdf
---
+++ test/radiodns.js
@@ -0,0 +1,193 @@
+'use strict'
+
+var assert = require('assert')
+var RadioDNS = require('../lib/radiodns')
+
+describe('RadioDNS', function () {
+
+  describe('constructFQDN', function () {
+    var radiodns
+    beforeEach(function () {
+      radiodns = new RadioDNS()
+    })
+
+    describe('for FM/VHF system', function () {
+      it('should construct a fqdn when freq is supplied as a number', function () {
+        var params = {
+          'system': 'fm',
+          'gcc': 'ce1',
+          'pi': 'c585',
+          'freq': 95.8
+        }
+        var fqdn = radiodns.constructFQDN(params)
+        assert.equal('09580.c585.ce1.fm.radiodns.org', fqdn)
+      })
+
+      it('should construct a fqdn when freq is a string', function () {
+        var params = {
+          'system': 'fm',
+          'gcc': 'ce1',
+          'pi': 'c585',
+          'freq': '09580'
+        }
+        var fqdn = radiodns.constructFQDN(params)
+        assert.equal('09580.c585.ce1.fm.radiodns.org', fqdn)
+      })
+
+      it('should throw if parameters are missing', function () {
+        var params = {
+          'system': 'fm',
+          'freq': '09580'
+        }
+        assert.throws(
+          function () {
+            radiodns.constructFQDN(params)
+          },
+          /Missing parameter when constructing FM FQDN/
+        )
+      })
+    })
+
+    describe('for DAB system', function () {
+      it('should construct a fqdn', function () {
+        var params = {
+          'system': 'dab',
+          'gcc': 'ce1',
+          'eid': 'c18c',
+          'sid': 'cc86',
+          'scids': '0'
+        }
+        var fqdn = radiodns.constructFQDN(params)
+        assert.equal('0.cc86.c18c.ce1.dab.radiodns.org', fqdn)
+      })
+
+      it('should construct a fqdn without a scids', function () {
+        var params = {
+          'system': 'dab',
+          'gcc': 'ce1',
+          'eid': 'c18c',
+          'sid': 'cc86'
+        }
+        var fqdn = radiodns.constructFQDN(params)
+        assert.equal('0.cc86.c18c.ce1.dab.radiodns.org', fqdn)
+      })
+
+      it('should prepend uatype if provided', function () {
+        var params = {
+          'system': 'dab',
+          'gcc': 'ce1',
+          'eid': 'c185',
+          'sid': 'e1c00098',
+          'scids': 0,
+          'uatype': '004'
+        }
+        var fqdn = radiodns.constructFQDN(params)
+        assert.equal('004.0.e1c00098.c185.ce1.dab.radiodns.org', fqdn)
+      })
+
+      it('should throw if parameters are missing', function () {
+        var params = {
+          'system': 'dab',
+          'gcc': 'ce1'
+        }
+        assert.throws(
+          function () {
+            radiodns.constructFQDN(params)
+          },
+          /Missing parameter when constructing DAB FQDN/
+        )
+      })
+    })
+
+    describe('for DRM system', function () {
+      it('should construct a fqdn', function () {
+        var params = {
+          'system': 'drm',
+          'sid': 'e1c238'
+        }
+        var fqdn = radiodns.constructFQDN(params)
+        assert.equal('e1c238.drm.radiodns.org', fqdn)
+      })
+
+      it('should throw if sid is missing', function () {
+        var params = {
+          'system': 'drm'
+        }
+        assert.throws(
+          function () {
+            radiodns.constructFQDN(params)
+          },
+          /Missing parameter when constructing DRM FQDN/
+        )
+      })
+    })
+
+    describe('for AMSS system', function () {
+      it('should construct a fqdn', function () {
+        var params = {
+          'system': 'amss',
+          'sid': 'sid'
+        }
+        var fqdn = radiodns.constructFQDN(params)
+        assert.equal('sid.amss.radiodns.org', fqdn)
+      })
+
+      it('should throw if sid is missing', function () {
+        var params = {
+          'system': 'amss'
+        }
+        assert.throws(
+          function () {
+            radiodns.constructFQDN(params)
+          },
+          /Missing parameter when constructing AMSS FQDN/
+        )
+      })
+    })
+
+    describe('for hd system', function () {
+      it('should construct a fqdn', function () {
+        var params = {
+          'system': 'hd',
+          'tx': 'tx',
+          'cc': 'cc'
+        }
+        var fqdn = radiodns.constructFQDN(params)
+        assert.equal('tx.cc.hd.radiodns.org', fqdn)
+      })
+
+      it('should throw if tx or cc is missing', function () {
+        var params = {
+          'system': 'hd'
+        }
+        assert.throws(
+          function () {
+            radiodns.constructFQDN(params)
+          },
+          /Missing parameter when constructing HD FQDN/
+        )
+      })
+    })
+
+    describe('with alternate root domain', function () {
+      var radiodns
+      beforeEach(function () {
+        radiodns = new RadioDNS('test.radiodns.org')
+      })
+
+      describe('for FM/VHF system', function () {
+        it('should construct a fqdn', function () {
+          var params = {
+            'system': 'fm',
+            'gcc': 'ce1',
+            'pi': 'c585',
+            'freq': '09580'
+          }
+          var fqdn = radiodns.constructFQDN(params)
+          assert.equal('09580.c585.ce1.fm.test.radiodns.org', fqdn)
+        })
+      })
+    })
+  })
+
+})
